fc4594a7dd7336d29550a514a3024505d44f27c5
kavishka chathumadura <145415943+Chathumadura@users.noreply.github.com>
2026-01-03 11:00:49 +0530

    create frontend , Backend and setup

---
+++ frontend/corn_app/lib/core/config/env.dart
@@ -0,0 +1,6 @@
+class Env {
+  // Backend base URL (Android emulator vs web vs real device)
+  // static const String baseUrl = 'http://10.0.2.2:8000';
+  // if testing from browser and backend on same machine: 'http://127.0.0.1:8000'
+  static const String baseUrl = 'http://127.0.0.1:8000';
+}
